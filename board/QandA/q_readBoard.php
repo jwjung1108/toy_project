be87db12cc92cabe7255db06db0feebc8c63004e
jwjung1108 <jwjung1108@naver.com>
2024-01-15 15:11:37 +0900

    a

--- board/QandA/q_readBoard.php
+++ board/QandA/q_readBoard.php
@@ -320,7 +320,7 @@ session_start();
                                 <?php echo $row['created']; ?>
                             </td>
                             <td>
-                                <a href="q_deleteComment.php?Number=<?php echo $row['number'] ?>">
+                                <a href="q_deleteComment.php?number=<?php echo $row['number'] ?>">
                                     <?php echo "삭제"; ?>
                                 </a>
                             </td>
