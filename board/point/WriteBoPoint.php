c3348c7cc6035c69299840889ed8b0d9e6ef2175
jwjung1108 <jwjung1108@naver.com>
2024-01-15 14:17:26 +0900

    a

--- board/point/WriteBoPoint.php
+++ board/point/WriteBoPoint.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-$userId = isset($_SESSION['userId']) ? $_SESSION['userId'] : '';
-if ($userId == '') {
+$userid = isset($_SESSION['UserID']) ? $_SESSION['UserID'] : '';
+if ($userid == '') {
     ?>
     <script>
         alert("로그인을 해주세요");
@@ -12,12 +12,12 @@ if ($userId == '') {
     exit();
 }
 
-$sql = "select point from users where id = '$userId'";
+$sql = "select point from users where id = '$userid'";
 $result = mysqli_fetch_array(mysqli_query($conn, $sql));
 
 $point = $result['point'] + 10;
 
-$sql = "update users set point = '$point' where id = '$userId'";
+$sql = "update users set point = '$point' where id = '$userid'";
 mysqli_query( $conn, $sql);
 
 ?>
