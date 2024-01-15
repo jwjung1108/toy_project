205fa001e88f5d4dbfc6f2b428144251906d1e9d
jwjung1108 <jwjung1108@naver.com>
2024-01-15 10:50:44 +0900

    noti

--- board/notification/list_nboard.php
+++ board/notification/list_nboard.php
@@ -135,7 +135,7 @@ $result = mysqli_query($conn, $sql);
 
                 <!-- Right-aligned links -->
                 <ul class="navbar-nav ms-auto">
-                    <?php if (isset($_SESSION['userId'])) { ?>
+                    <?php if (isset($_SESSION['UserID'])) { ?>
                         <li class="nav-item">
                             <a class="nav-link" href="/MyPage/mypage.php">마이페이지</a>
                         </li>
