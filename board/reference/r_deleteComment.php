63a9cf0ffd64637098892c89d20116d601820baf
jwjung1108 <jwjung1108@naver.com>
2024-01-16 17:45:16 +0900

    a

--- board/reference/r_deleteComment.php
+++ board/reference/r_deleteComment.php
@@ -1,58 +1,37 @@
 <?php
 include '../../connect.php';
+
 session_start();
-$number = $_GET['number'];
-$sql = "SELECT userid from r_comment where number= '$number'";
-$row = mysqli_fetch_array(mysqli_query($conn, $sql));
-
-?>
-
-<?php
-//사용자 권한 확인
 $userid = $_SESSION['UserID'];
-$check_user = $_SESSION['authority'];
 
-?>
-
-
-<!DOCTYPE html>
-<html lang="ko">
-
-<head>
-</head>
-
-<body>
+if ($_SESSION['authority'] != "admin") {
+    ?>
+    <script>
+        alert("접근 권한이 없습니다.");
+        location.href = "./list_rboard.php";
+    </script>
     <?php
-    if ($row['userid'] != $userid) {
-        if ($check_user != 'admin') {
-            ?>
-            <script>
-                alert("'접근 권한이 없습니다.';");
-                location.href = "list_rboard.php";
-            </script>
-            <?php
-            exit();
-        }
-        ?>
-        <?php
-    }
-    $sql = "select visible from r_comment where number = '$number' and visible = 1";
-    $row = mysqli_fetch_array(mysqli_query($conn, $sql));
+    exit();
+}
 
-    $result = mysqli_query($conn, $sql);
+$number = $_GET['number'];
 
-    if ($result) {
-        ?>
-        <script>
-            alert("댓글이 삭제되었습니다.");
-            location.href = "list_reference.php?=<?php ?>";
-        </script>
-        <?php
-    } ?>
+$sql = "DELETE FROM r_comment WHERE number = '$number'";
+$result = mysqli_query($conn, $sql);
 
-
-
-
-</body>
-
-</html>
+if ($result) {
+    ?>
+    <script>
+        alert("댓글이 삭제되었습니다.");
+        history.go(-1);
+    </script>
+    <?php
+} else {
+    ?>
+    <script>
+        alert("댓글 삭제에 실패하였습니다.");
+        history.go(-1);
+    </script>
+    <?php
+}
+?>
