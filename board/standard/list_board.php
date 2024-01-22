34789f26a3020dd169a44ac2767ae47f2a71a9c9
underkim <rlaehdgns4568@gmail.com>
2024-01-22 20:14:46 +0900

    자유게시판 css 주소 수정

--- board/standard/list_board.php
+++ board/standard/list_board.php
@@ -47,9 +47,9 @@ $result = mysqli_query($conn, $sql);
     <title>지원이의 산뜻한 페이지</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="../assets/css/main.css" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
     <noscript>
-        <link rel="stylesheet" href="../assets/css/noscript.css" />
+        <link rel="stylesheet" href="/assets/css/noscript.css" />
     </noscript>
 
     <style>
@@ -286,13 +286,13 @@ $result = mysqli_query($conn, $sql);
     </div>
 
     <!-- Scripts -->
-    <script src="../assets/js/jquery.min.js"></script>
-    <script src="../assets/js/jquery.scrollex.min.js"></script>
-    <script src="../assets/js/jquery.scrolly.min.js"></script>
-    <script src="../assets/js/browser.min.js"></script>
-    <script src="../assets/js/breakpoints.min.js"></script>
-    <script src="../assets/js/util.js"></script>
-    <script src="../assets/js/main.js"></script>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/jquery.scrollex.min.js"></script>
+    <script src="/assets/js/jquery.scrolly.min.js"></script>
+    <script src="/assets/js/browser.min.js"></script>
+    <script src="/assets/js/breakpoints.min.js"></script>
+    <script src="/assets/js/util.js"></script>
+    <script src="/assets/js/main.js"></script>
 
 </body>
 
