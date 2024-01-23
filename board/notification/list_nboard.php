fb5414403f540b4263bc6d643e7ab1bc154f956e
underkim <rlaehdgns4568@gmail.com>
2024-01-23 20:53:51 +0900

    qboard,nboard,rboard 수정

--- board/notification/list_nboard.php
+++ board/notification/list_nboard.php
@@ -42,52 +42,15 @@ $result = mysqli_query($conn, $sql);
 <html lang="ko">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>지원이의 산뜻한 페이지</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="/assets/css/main.css" />
+    <noscript>
+        <link rel="stylesheet" href="/assets/css/noscript.css" />
+    </noscript>
 
-    <!-- script -->
-    <script src='../js/checkbox.js'></script>
-
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="/board/css/style.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-
-    <title>공지사항</title>
     <style>
-        body {
-            padding-top: 50px;
-        }
-
-        .container {
-            max-width: 960px;
-        }
-
-        .table {
-            margin-bottom: 1rem;
-            color: #212529;
-        }
-
-        .important {
-            background-color: #00000026;
-            /* 중요 공지사항 배경색 */
-            color: #000;
-            /* 텍스트 색상 */
-            font-weight: bold;
-            /* 글꼴 두껍게 */
-            border: 2px solid #00000026;
-            /* 테두리 스타일과 색상 */
-            border-radius: 5px;
-            /* 둥근 테두리 */
-        }
-
-        .generic {}
-
         .tier-icon {
             width: 20px;
             /* 이미지의 크기 조절 */
@@ -97,181 +60,245 @@ $result = mysqli_query($conn, $sql);
             /* 티어 아이콘 간의 간격 조절 */
         }
     </style>
-</head>
-
-<body>
-    <!-- Navbar 시작 -->
-    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
-        <div class="container-fluid">
-            <!-- Navbar Brand -->
-            <a class="navbar-brand" href="#">공지사항</a>
-
-            <!-- Toggler -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <!-- Navbar Links -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <!-- Center-aligned links -->
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">홈</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/board/standard/list_board.php">자유게시판</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/board/notification/list_nboard.php">공지사항</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/board/QandA/list_qboard.php">Q&A</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/board/reference/list_rboard.php">자료실</a>
-                    </li>
-                </ul>
-
-                <!-- Right-aligned links -->
-                <ul class="navbar-nav ms-auto">
-                    <?php if (isset($_SESSION['UserID'])) { ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/MyPage/mypage.php">마이페이지</a>
-                        </li>
-                        <?php if ($_SESSION['authority'] == 'admin') { ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/adminPage/adminpage.php">관리자페이지</a>
-                            </li>
-                        <?php } ?>
-                        <li class="nav-item">
-                            <button class="btn btn-outline-secondary" onclick="logout()">로그아웃</button>
-                        </li>
-                    <?php } else { ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/join/login.php">로그인</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/join/signup.php">회원가입</a>
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container" style="margin-top: 80px;">
-        <h1 class="text-center">공지사항</h1>
-        <div class="text-end mb-3">
-            <a href="?sort=views" class="btn btn-primary <?php echo ($sort == 'views') ? 'active' : ''; ?>">조회수</a>
-            <a href="?sort=number" class="btn btn-primary <?php echo ($sort == 'number') ? 'active' : ''; ?>">순번</a>
-        </div>
-
-
-        <!-- 검색 -->
-        <div id="search_box">
-            <form action="n_search_result.php" method="get">
-                <select name="catgo">
-                    <option value="title">제목</option>
-                    <option value="nickname">글쓴이</option>
-                    <option value="board">내용</option>
-                </select>
-                <input type="text" name="search" required="required" />
-                <button>검색</button>
-            </form>
-        </div>
-
-
-
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">번호</th>
-                        <th scope="col">제목</th>
-                        <th scope="col">작성자</th>
-                        <th scope="col">등록일</th>
-                        <th scope="col">조회수</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $i = 1;
-                    while ($row = mysqli_fetch_array($result)) {
-                        $class = '';
-                        if ($row['important'] == 1) {
-                            $class = 'important';
-                        } elseif ($row['important'] == 0) {
-                            $class = 'generic';
-                        }
-                        $authorRank = $row['user_rank'];
-                        $tierIconPath = isset($tierIcons[$authorRank]) ? $tierIcons[$authorRank] : $tierIcons['Default'];
-
-                        // Determine color based on rank
-                        switch ($authorRank) {
-                            case 'Bronze':
-                                $color = 'color: #cd7f32;'; // Bronze color (e.g., brown)
-                                break;
-                            case 'Silver':
-                                $color = 'color: #c0c0c0;'; // Silver color (e.g., silver)
-                                break;
-                            case 'Gold':
-                                $color = 'color: #ffd700;'; // Gold color (e.g., gold)
-                                break;
-                            case 'Platinum':
-                                $color = 'color: #ff4500;'; // Platinum color (e.g., orange)
-                                break;
-                            case 'Master':
-                                $color = 'color: #ff8c00;'; // Master color (e.g., orange)
-                                break;
-                            default:
-                                $color = 'color: black;'; // Default color (e.g., black)
-                                break;
-                        }
-
-                        ?>
-                        <tr class='<?php echo $class; ?>'>
-                            <th scope="row">
-                                <?php echo $i++; ?>
-                            </th>
-                            <td class="title-cell"><a href="n_readBoard.php?number=<?php echo $row['number']; ?>">
-                                    <?php echo $row['title']; ?>
-                                </a>
-                            </td>
-                            <td class="title-cell" style="<?php echo $color; ?>">
-                                <img src="<?php echo $tierIconPath; ?>" alt="tier" class="tier-icon" />
-                                <?php echo $row['nickname']; ?>
-                            </td>
-                            <td class="title-cell">
-                                <?php echo $row['created']; ?>
-                            </td>
-                            <td>
-                                <?php echo $row['views']; ?>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-        <div class="text-center">
-            <?php
-            if ($_SESSION['authority'] == 'admin') {
-                ?><a href="n_writeForm.php" class="btn btn-primary">작성</a>
-                <?php
-            }
-            ?>
-
-            <a href="/" class="btn btn-secondary">목록으로 돌아가기</a>
-        </div>
-    </div>
     <script>
+        function goToLoginPage() {
+            window.location.href = "/join/login.php";
+        }
+        function goToSignupPage() {
+            window.location.href = "/join/signup.php";
+        }
+        function goTocommonBoardPage() {
+            window.location.href = "/board/standard/list_board.php";
+        }
+        function goTonotificationBoardPage() {
+            window.location.href = "/board/notification/list_nboard.php";
+        }
+        function goToQandABoardPage() {
+            window.location.href = "/board/QandA/list_qboard.php";
+        }
+        function goToReferencePage() {
+            window.location.href = "/board/reference/list_rboard.php";
+        }
         function logout() {
             const data = confirm("로그아웃 하시겠습니까?");
             if (data) {
                 location.href = "/join/logoutProcess.php";
             }
-        } 
+        }
+        function goToadminPage() {
+            window.location.href = "/adminPage/adminpage.php";
+        }
+        function goToMyPage() {
+            window.location.href = "/MyPage/mypage.php";
+        }
     </script>
+
+
+</head>
+
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper" class="fade-in">
+
+        <!-- Intro -->
+        <div id="intro">
+            <h1>지원이 최고야</h1>
+            <p>보기싫으면 밑으로!</p>
+            <ul class="actions">
+                <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">계속</a></li>
+            </ul>
+        </div>
+
+        <!-- Header -->
+        <header id="header">
+            <a href="/index.php" class="logo">페이지 제목</a>
+        </header>
+
+        <!-- Nav -->
+        <nav id="nav">
+
+            <ul class="links">
+                <li><a href="/index.php">메인</a></li>
+                <li class="active"><a href="/board/notification/list_nboard.php">공지사항</a></li>
+                <li><a href="/board/standard/list_board.php">자유게시판</a></li>
+                <li><a href="/board/QandA/list_rboard.php">자료실</a></li>
+                <li><a href="/board/QandA/list_qboard.php">Q&A</a></li>
+                <?php if (isset($_SESSION['UserID'])) { ?>
+                    <?php if ($_SESSION['authority'] == 'admin') { ?>
+                        <li><a href="/adminPage/adminpage.php">관리자페이지</a></li>
+                    <?php } else { ?>
+                        <li><a href="/MyPage/mypage.php">마이페이지</a></li>
+                    <?php } ?>
+                    <li><a onclick="logout()">로그아웃</a></li>
+
+                <?php } else { ?>
+                    <li><a href="/join/login.php">로그인</a></li>
+                    <li><a href="/join/signup.php">회원가입</a></li>
+                <?php } ?>
+            </ul>
+
+
+        </nav>
+
+        <!-- Main -->
+        <div id="main">
+
+            <!-- Featured Post -->
+            <article class="post featured">
+                <header class="major">
+                    <h2><a href="#">제목<br />
+                            이에요</a></h2>
+                    <p>사이트 설명.</p>
+                </header>
+                <a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+            </article>
+
+            <!-- Posts -->
+            <section class="post">
+                <h1>공지사항</h1>
+                <div class="text-end mb-3">
+                    <a href="?sort=views" <?php echo ($sort == 'views') ? 'active' : ''; ?>">조회수</a>
+                    <a href="?sort=number" <?php echo ($sort == 'number') ? 'active' : ''; ?>">순번</a>
+                </div>
+
+
+                <!-- 검색 -->
+                <div id="search_box">
+                    <form action="n_search_result.php" method="get">
+                        <select name="catgo">
+                            <option value="title">제목</option>
+                            <option value="nickname">글쓴이</option>
+                            <option value="board">내용</option>
+                        </select>
+                        <input type="text" name="search" required="required" />
+                        <button>검색</button>
+                    </form>
+                </div>
+
+
+
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">번호</th>
+                                <th scope="col">제목</th>
+                                <th scope="col">작성자</th>
+                                <th scope="col">등록일</th>
+                                <th scope="col">조회수</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            while ($row = mysqli_fetch_array($result)) {
+                                $class = '';
+                                if ($row['important'] == 1) {
+                                    $class = 'important';
+                                } elseif ($row['important'] == 0) {
+                                    $class = 'generic';
+                                }
+                                $authorRank = $row['user_rank'];
+                                $tierIconPath = isset($tierIcons[$authorRank]) ? $tierIcons[$authorRank] : $tierIcons['Default'];
+
+                                // Determine color based on rank
+                                switch ($authorRank) {
+                                    case 'Bronze':
+                                        $color = 'color: #cd7f32;'; // Bronze color (e.g., brown)
+                                        break;
+                                    case 'Silver':
+                                        $color = 'color: #c0c0c0;'; // Silver color (e.g., silver)
+                                        break;
+                                    case 'Gold':
+                                        $color = 'color: #ffd700;'; // Gold color (e.g., gold)
+                                        break;
+                                    case 'Platinum':
+                                        $color = 'color: #ff4500;'; // Platinum color (e.g., orange)
+                                        break;
+                                    case 'Master':
+                                        $color = 'color: #ff8c00;'; // Master color (e.g., orange)
+                                        break;
+                                    default:
+                                        $color = 'color: black;'; // Default color (e.g., black)
+                                        break;
+                                }
+
+                                ?>
+                                <tr class='<?php echo $class; ?>'>
+                                    <th scope="row">
+                                        <?php echo $i++; ?>
+                                    </th>
+                                    <td><a href="n_readBoard.php?number=<?php echo $row['number']; ?>">
+                                            <?php echo $row['title']; ?>
+                                        </a>
+                                    </td>
+                                    <td style="<?php echo $color; ?>">
+                                        <img src="<?php echo $tierIconPath; ?>" alt="tier" class="tier-icon" />
+                                        <?php echo $row['nickname']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['created']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row['views']; ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <?php
+                    if ($_SESSION['authority'] == 'admin') {
+                        ?><a href="n_writeForm.php">작성</a>
+                        <?php
+                    }
+                    ?>
+
+                    <a href="/">목록으로 돌아가기</a>
+                </div>
+
+
+        </div>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <section class="split contact">
+                <section class="alt">
+                    <h3>무슨</h3>
+                    <p>설명적는곳</p>
+                </section>
+                <section>
+                    <h3>무슨</h3>
+                    <p><a href="#">설명적는곳</a></p>
+                </section>
+                <section>
+                    <h3>무슨</h3>
+                    <p><a href="#">설명적는곳</a></p>
+                </section>
+            </section>
+        </footer>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            <ul>
+                <li>&copy; Untitled</li>
+                <li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/jquery.scrollex.min.js"></script>
+    <script src="/assets/js/jquery.scrolly.min.js"></script>
+    <script src="/assets/js/browser.min.js"></script>
+    <script src="/assets/js/breakpoints.min.js"></script>
+    <script src="/assets/js/util.js"></script>
+    <script src="/assets/js/main.js"></script>
+
 </body>
 
 </html>
