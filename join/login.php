dd1463693f854c48f7ce0b9c72b512b8faf8ae4b
underkim <rlaehdgns4568@gmail.com>
2024-01-31 16:42:27 +0900

    로그인 페이지

--- join/login.php
+++ join/login.php
@@ -2,29 +2,159 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-    <link href="/board/css/style.css" rel="stylesheet">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+    <title>지원이의 산뜻한 페이지</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css" />
+    </noscript>
+
+    <script>
+        function goToLoginPage() {
+            window.location.href = "/join/login.php";
+        }
+        function goToSignupPage() {
+            window.location.href = "/join/signup.php";
+        }
+        function goTocommonBoardPage() {
+            window.location.href = "/board/standard/list_board.php";
+        }
+        function goTonotificationBoardPage() {
+            window.location.href = "/board/notification/list_nboard.php";
+        }
+        function goToQandABoardPage() {
+            window.location.href = "/board/QandA/list_qboard.php";
+        }
+        function goToReferencePage() {
+            window.location.href = "/board/reference/list_rboard.php";
+        }
+        function logout() {
+            const data = confirm("로그아웃 하시겠습니까?");
+            if (data) {
+                location.href = "/join/logoutProcess.php";
+            }
+        }
+        function goToadminPage() {
+            window.location.href = "/adminPage/adminpage.php";
+        }
+        function goToMyPage() {
+            window.location.href = "/MyPage/mypage.php";
+        }
+    </script>
 </head>
 
-<body>
-    <form method="POST" action="/join/loginProcess.php"> 
-    <div class="w-50 ml-auto mr-auto mt-5">
-        <div class="mb-3 ">
-            <label for="exampleFormControlInput1" class="form-label">아이디</label>
-            <input name="id" type="id" class="form-control" id="exampleFormControlInput1" placeholder="id">
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper" class="fade-in">
+
+
+        <!-- Header -->
+        <header id="header">
+            <a href="/index.php" class="logo">페이지 제목</a>
+        </header>
+
+        <!-- Nav -->
+        <nav id="nav">
+
+            <ul class="links">
+                <li><a href="/index.php">메인</a></li>
+                <li><a href="/board/notification/list_nboard.php">공지사항</a></li>
+                <li><a href="/board/standard/list_board.php">자유게시판</a></li>
+                <li><a href="/board/reference/list_rboard.php">자료실</a></li>
+                <li><a href="/board/QandA/list_qboard.php">Q&A</a></li>
+            </ul>
+            <ul class="links" style="flex-grow:0;">
+                <?php if (isset($_SESSION['UserID'])) { ?>
+                    <?php if ($_SESSION['authority'] == 'admin') { ?>
+                        <li><a href="/adminPage/adminpage.php">관리자페이지</a></li>
+                    <?php } else { ?>
+                        <li><a href="/MyPage/mypage.php">마이페이지</a></li>
+                    <?php } ?>
+                    <li><a onclick="logout()">로그아웃</a></li>
+
+                <?php } else { ?>
+                    <li class="active"><a href="/join/login.php">로그인</a></li>
+                    <li><a href="/join/signup.php">회원가입</a></li>
+                <?php } ?>
+            </ul>
+
+
+        </nav>
+
+        <!-- Main -->
+        <div id="main">
+
+            <!-- Featured Post -->
+            <article class="post featured">
+                <header class="major">
+                    <h2><a href="#">제목<br />
+                            이에요</a></h2>
+                    <p>사이트 설명.</p>
+
+                </header>
+                <a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+            </article>
+
+            <!-- Posts -->
+            <section class="posts">
+                <form method="POST" action="/join/loginProcess.php">
+                    <div>
+                        <div>
+                            <label for="exampleFormControlInput1">아이디</label>
+                            <input name="id" type="id" id="exampleFormControlInput1" placeholder="id">
+                        </div>
+                        <div">
+                            <label for="exampleFormControlInput1">비밀번호</label>
+                            <input name="password" type="password" id="exampleFormControlInput1" placeholder="password">
+                    </div>
+
+                    <button type="submit">접속</button>
         </div>
-        <div class="mb-3 ">
-            <label for="exampleFormControlInput1" class="form-label">비밀번호</label>
-            <input name="password" type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
-        </div>
-    
-        <button type="submit" class="btn btn-primary mb-3">접속</button>
+        </form>
+        </section>
+
+
     </div>
-    </form>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <section class="split contact">
+            <section class="alt">
+                <h3>무슨</h3>
+                <p>설명적는곳</p>
+            </section>
+            <section>
+                <h3>무슨</h3>
+                <p><a href="#">설명적는곳</a></p>
+            </section>
+            <section>
+                <h3>무슨</h3>
+                <p><a href="#">설명적는곳</a></p>
+            </section>
+        </section>
+    </footer>
+
+    <!-- Copyright -->
+    <div id="copyright">
+        <ul>
+            <li>&copy; Untitled</li>
+            <li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+        </ul>
+    </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
 </body>
 
 </html>
